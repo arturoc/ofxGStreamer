d918e3296733a6eac7d8f25505075c8c6dd2baa8
arturo <arturo@openframeworks.cc>
2013-10-12 13:47:20 +0200

    remove default grabber and player defaults, won't work unless recompiling full OF as dependency of the project through makefiles

--- addon_config.mk
+++ addon_config.mk
@@ -33,8 +33,7 @@ common:
 	
 	# any special flag that should be passed to the compiler when using this
 	# addon
-	ADDON_CFLAGS = -DOF_VIDEO_CAPTURE_GSTREAMER
-	ADDON_CFLAGS += -DOF_VIDEO_PLAYER_GSTREAMER 
+	# ADDON_CFLAGS = 
 	
 	# any special flag that should be passed to the linker when using this
 	# addon, also used for system libraries with -lname
