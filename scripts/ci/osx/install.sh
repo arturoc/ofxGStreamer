0ad66f0ad148a805c3df98442c0e2524bf088b74
arturo castro <arturo@openframeworks.cc>
2016-12-10 19:51:19 +0100

    patch for 0.9 ci

--- scripts/ci/osx/install.sh
+++ scripts/ci/osx/install.sh
@@ -7,5 +7,5 @@ sudo installer -pkg gstreamer-1.0-1.9.90-x86_64.pkg -target /
 ls /Library/Frameworks/GStreamer.framework/Headers
 
 if [ "$OF_BRANCH" == "stable" ]; then
-    sed "s/$(CXX) -c $(OPTIMIZATION_CFLAGS) $(CFLAGS) $(CXXFLAGS) $(OF_CORE_INCLUDES_CFLAGS) -MMD/$(CXX) -c $(OPTIMIZATION_CFLAGS) $(CFLAGS) $(CXXFLAGS) $(OF_CORE_INCLUDES_CFLAGS) $(ADDON_INCLUDE_CFLAGS) -MMD/g;" ~/openFrameworks/libs/openFrameworksCompiled/project/makefileCommon/compile.project.mk
+    sed -i "s/$(CXX) -c $(OPTIMIZATION_CFLAGS) $(CFLAGS) $(CXXFLAGS) $(OF_CORE_INCLUDES_CFLAGS) -MMD/$(CXX) -c $(OPTIMIZATION_CFLAGS) $(CFLAGS) $(CXXFLAGS) $(OF_CORE_INCLUDES_CFLAGS) $(ADDON_INCLUDE_CFLAGS) -MMD/g;" ~/openFrameworks/libs/openFrameworksCompiled/project/makefileCommon/compile.project.mk
 fi
