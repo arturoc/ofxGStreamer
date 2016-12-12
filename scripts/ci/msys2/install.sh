65bd3b96dba5b7b5e5952a51a45293b660298148
arturo castro <arturo@openframeworks.cc>
2016-12-12 20:44:55 +0100

    fix appveyor

--- scripts/ci/msys2/install.sh
+++ scripts/ci/msys2/install.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-pacman -S mingw-w64-i686-gstreamer mingw-w64-i686-gst-plugins-base mingw-w64-i686-gst-plugins-good
+pacman --noconfirm -S mingw-w64-i686-gstreamer mingw-w64-i686-gst-plugins-base mingw-w64-i686-gst-plugins-good
