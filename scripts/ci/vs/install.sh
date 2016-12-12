573e89f24a596c66ece6c699ac6c894e2d10e2c2
arturo castro <arturo@openframeworks.cc>
2016-12-12 22:31:35 +0100

    remove logging from appveyor config

--- scripts/ci/vs/install.sh
+++ scripts/ci/vs/install.sh
@@ -5,8 +5,8 @@ echo "Downloading https://gstreamer.freedesktop.org/data/pkg/windows/1.10.2/gstr
 wget https://gstreamer.freedesktop.org/data/pkg/windows/1.10.2/gstreamer-1.0-x86_64-1.10.2.msi 2> /dev/null
 echo "Installing gstreamer-1.0-devel-x86_64-1.10.2.msi"
 cmd /c "msiexec /L* install_devel.log /i gstreamer-1.0-devel-x86_64-1.10.2.msi /quiet"
-cat install_devel.log
+#cat install_devel.log
 echo "Installing gstreamer-1.0-x86_64-1.10.2.msi"
 cmd /c "msiexec /L* install.log /i gstreamer-1.0-x86_64-1.10.2.msi /quiet"
-cat install.log
+#cat install.log
 
