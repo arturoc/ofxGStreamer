9ad5bf10aa8dd6ede1f7cc366fe7b17c2bf9202e
arturo castro <arturo@openframeworks.cc>
2016-12-12 18:51:00 +0100

    fix appveyor and update osx gstreamer version for travis

--- scripts/ci/osx/install.sh
+++ scripts/ci/osx/install.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
-wget https://gstreamer.freedesktop.org/data/pkg/osx/1.9.90/gstreamer-1.0-devel-1.9.90-x86_64.pkg
-sudo installer -pkg gstreamer-1.0-devel-1.9.90-x86_64.pkg -target /
-wget https://gstreamer.freedesktop.org/data/pkg/osx/1.9.90/gstreamer-1.0-1.9.90-x86_64.pkg
-sudo installer -pkg gstreamer-1.0-1.9.90-x86_64.pkg -target /
+wget https://gstreamer.freedesktop.org/data/pkg/osx/1.9.90/gstreamer-1.0-devel-1.10.2-x86_64.pkg
+sudo installer -pkg gstreamer-1.0-devel-1.10.2-x86_64.pkg -target /
+wget https://gstreamer.freedesktop.org/data/pkg/osx/1.9.90/gstreamer-1.0-1.10.2-x86_64.pkg
+sudo installer -pkg gstreamer-1.0-1.10.2-x86_64.pkg -target /
 
 if [ "$OF_BRANCH" == "stable" ]; then
     sed -ie 's/$(CXX) -c $(OPTIMIZATION_CFLAGS) $(CFLAGS) $(CXXFLAGS) $(OF_CORE_INCLUDES_CFLAGS) -MMD/$(CXX) -c $(OPTIMIZATION_CFLAGS) $(CFLAGS) $(CXXFLAGS) $(OF_CORE_INCLUDES_CFLAGS) $(ADDON_INCLUDE_CFLAGS) -MMD/g;' ~/openFrameworks/libs/openFrameworksCompiled/project/makefileCommon/compile.project.mk
