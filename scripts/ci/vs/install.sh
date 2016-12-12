925eed6ff30f36c17803c781e25c199ba18b2f20
arturo castro <arturo@openframeworks.cc>
2016-12-12 19:00:49 +0100

    fix appveyor

--- scripts/ci/vs/install.sh
+++ scripts/ci/vs/install.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-wget https://gstreamer.freedesktop.org/data/pkg/windows/1.10.2/gstreamer-1.0-devel-x86_64-1.10.2.msi
-wget https://gstreamer.freedesktop.org/data/pkg/windows/1.10.2/gstreamer-1.0-x86_64-1.10.2.msi
-msiexec /package gstreamer-1.0-devel-x86_64-1.10.2.msi /quite /norestart
-msiexec /package gstreamer-1.0-x86_64-1.10.2.msi /quite /norestart
+wget https://gstreamer.freedesktop.org/data/pkg/windows/1.10.2/gstreamer-1.0-devel-x86_64-1.10.2.msi 2> /dev/null
+wget https://gstreamer.freedesktop.org/data/pkg/windows/1.10.2/gstreamer-1.0-x86_64-1.10.2.msi 2> /dev/null
+msiexec /package gstreamer-1.0-devel-x86_64-1.10.2.msi /quiet /norestart
+msiexec /package gstreamer-1.0-x86_64-1.10.2.msi /quiet /norestart
