7d3e5c400928f6975dc937d553fb5a8a93b418ee
arturo castro <arturo@openframeworks.cc>
2016-12-13 18:02:50 +0100

    appveyor: trying to set vars from bash script

--- scripts/ci/osx/install.sh
+++ scripts/ci/osx/install.sh
@@ -8,3 +8,5 @@ sudo installer -pkg gstreamer-1.0-1.10.2-x86_64.pkg -target /
 if [ "$OF_BRANCH" == "stable" ]; then
     sed -ie 's/$(CXX) -c $(OPTIMIZATION_CFLAGS) $(CFLAGS) $(CXXFLAGS) $(OF_CORE_INCLUDES_CFLAGS) -MMD/$(CXX) -c $(OPTIMIZATION_CFLAGS) $(CFLAGS) $(CXXFLAGS) $(OF_CORE_INCLUDES_CFLAGS) $(ADDON_INCLUDE_CFLAGS) -MMD/g;' ~/openFrameworks/libs/openFrameworksCompiled/project/makefileCommon/compile.project.mk
 fi
+
+export GSTREAMER_1_0_ROOT_X86_64=C:\\gstreamer\\1.0\\x86_64\\
